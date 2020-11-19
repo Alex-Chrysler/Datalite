bd3073cf97fe371de9148c3a3cffa4e9f447ad07
Alex Chrysler <achrysler36@gmail.com>
2020-11-19 16:22:00 -0600

    Initial commit

---
+++ src/main/java/com/example/Datalite/DataliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Datalite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Datalite/DataliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Datalite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
